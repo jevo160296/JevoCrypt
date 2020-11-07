0206f290f17609af4c49eafb2b9a12901a074e75
Jose Miguel Millan Londoño <jevo160296@gmail.com>
2020-11-07 13:42:53 -0500

    Agregar archivos de proyecto.

---
+++ Crypto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JevoCrypt
+{
+    public static class Crypto
+    {
+        public static string Encrypt(string text, string key)
+        {
+
+            RijndaelManaged objrij = new RijndaelManaged();
+            //set the mode for operation of the algorithm
+            objrij.Mode = CipherMode.CBC;
+            //set the padding mode used in the algorithm.
+            objrij.Padding = PaddingMode.PKCS7;
+            //set the size, in bits, for the secret key.
+            objrij.KeySize = 0x80;
+            //set the block size in bits for the cryptographic operation.
+            objrij.BlockSize = 0x80;
+            //set the symmetric key that is used for encryption & decryption.
+            byte[] passBytes = Encoding.UTF8.GetBytes(key);
+            //set the initialization vector (IV) for the symmetric algorithm
+            byte[] EncryptionkeyBytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            int len = passBytes.Length;
+            if (len > EncryptionkeyBytes.Length)
+            {
+                len = EncryptionkeyBytes.Length;
+            }
+            Array.Copy(passBytes, EncryptionkeyBytes, len);
+            objrij.Key = EncryptionkeyBytes;
+            objrij.IV = EncryptionkeyBytes;
+            //Creates symmetric AES object with the current key and initialization vector IV.
+            ICryptoTransform objtransform = objrij.CreateEncryptor();
+            byte[] textDataByte = Encoding.UTF8.GetBytes(text);
+            //Final transform the test string.
+            return Convert.ToBase64String(objtransform.TransformFinalBlock(textDataByte, 0, textDataByte.Length));
+        }
+        public static string Decrypt(string text, string key)
+        {
+            RijndaelManaged objrij = new RijndaelManaged();
+            objrij.Mode = CipherMode.CBC;
+            objrij.Padding = PaddingMode.PKCS7;
+            objrij.KeySize = 0x80;
+            objrij.BlockSize = 0x80;
+            byte[] encryptedTextByte = Convert.FromBase64String(text);
+            byte[] passBytes = Encoding.UTF8.GetBytes(key);
+            byte[] EncryptionkeyBytes = new byte[0x10];
+            int len = passBytes.Length;
+            if (len > EncryptionkeyBytes.Length)
+            {
+                len = EncryptionkeyBytes.Length;
+            }
+            Array.Copy(passBytes, EncryptionkeyBytes, len);
+            objrij.Key = EncryptionkeyBytes;
+            objrij.IV = EncryptionkeyBytes;
+            byte[] TextByte = objrij.CreateDecryptor().TransformFinalBlock(encryptedTextByte, 0, encryptedTextByte.Length);
+            return Encoding.UTF8.GetString(TextByte);  //it will return readable string
+        }
+    }
+}
